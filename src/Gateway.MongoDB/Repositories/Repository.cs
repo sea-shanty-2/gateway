0b170e515021c82cf7b918cea6835e11ba6df129
Mikkel Jarlund <mikkel.jarlund@gmail.com>
2019-05-20 10:03:03 +0200

    removed thread sleep from mongo repository

--- src/Gateway.MongoDB/Repositories/Repository.cs
+++ src/Gateway.MongoDB/Repositories/Repository.cs
@@ -23,13 +23,12 @@ namespace Gateway.MongoDB.Repositories
             /// Ignore default values
             /// http://mongodb.github.io/mongo-csharp-driver/2.4/reference/bson/mapping/conventions/
             ConventionRegistry.Register("IgnoreIfNull",
-                            new ConventionPack { 
-                                new IgnoreIfNullConvention(true) 
+                            new ConventionPack {
+                                new IgnoreIfNullConvention(true)
                             },
                             t => true);
 
             /// Map string id to objectid (required for unique indexing in mongodb).
-            /// It is very important that the registration of class maps occur prior to them being needed.
             /// http://mongodb.github.io/mongo-csharp-driver/2.2/reference/bson/mapping/
             BsonClassMap.RegisterClassMap<T>(cm =>
             {
@@ -71,107 +70,48 @@ namespace Gateway.MongoDB.Repositories
 
         public async Task<T> FindAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default)
         {
-            T entity = default;
-
-            for (var attempt = 0; attempt < 3; attempt++)
-            {
-                var cursor = await _collection.FindAsync(
-                    expression,
-                    new FindOptions<T, T>
-                    {
-                        Limit = 1
-                    },
-                    cancellationToken
-                );
-                entity = await cursor.FirstOrDefaultAsync(cancellationToken);
-
-                if (entity != null)
+            var cursor = await _collection.FindAsync(
+                expression,
+                new FindOptions<T, T>
                 {
-                    break;
-                }
-                else
-                {
-                    Thread.Sleep(TimeSpan.FromSeconds(1));
-                }
-            }
+                    Limit = 1
+                },
+                cancellationToken
+            );
 
-            return entity;
+            return await cursor.FirstOrDefaultAsync();
         }
 
         public async Task<IEnumerable<T>> FindRangeAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken)
         {
-            IEnumerable<T> entities = default;
-
-            for (var attempt = 0; attempt < 3; attempt++)
-            {
-                entities = await Task.Run(() => _collection.AsQueryable().Where(expression).AsEnumerable(), cancellationToken);
-
-                if (entities.Any())
-                {
-                    break;
-                }
-                else
-                {
-                    Thread.Sleep(TimeSpan.FromSeconds(1));
-                }
-            }
-
-            return entities;
+            return await Task.Run(() => _collection.AsQueryable().Where(expression).AsEnumerable(), cancellationToken);
         }
 
 
         public async Task RemoveAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default)
         {
-            DeleteResult deletion = default;
-
-            for (int attempt = 0; attempt < 3; attempt++)
-            {
-                deletion = await _collection.DeleteOneAsync(
-                    expression,
-                    new DeleteOptions
-                    {
-
-                    },
-                    cancellationToken
-                );
-
-                if (deletion.IsAcknowledged)
+            await _collection.DeleteOneAsync(
+                expression,
+                new DeleteOptions
                 {
-                    break;
-                }
-                else
-                {
-                    Thread.Sleep(TimeSpan.FromSeconds(1));
-                }
-            }
+
+                },
+                cancellationToken
+            );
 
             return;
         }
 
         public async Task RemoveRangeAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default)
         {
-            DeleteResult deletion = default;
-
-            for (int attempt = 0; attempt < 3; attempt++)
-            {
-                deletion = await _collection.DeleteManyAsync(
-                    expression,
-                    new DeleteOptions
-                    {
-                        
-                    },
-                    cancellationToken
-                );
-
-                if (deletion.IsAcknowledged)
+            await _collection.DeleteManyAsync(
+                expression,
+                new DeleteOptions
                 {
-                    break;
-                }
-                else
-                {
-                    Thread.Sleep(TimeSpan.FromSeconds(1));
-                }
-            }
+
+                },
+                cancellationToken
+            );
 
             return;
         }
